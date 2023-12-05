4c4c4b5c8f00593e9e0728713b16ea36c13764ee
Brian Aston <astonb.dev@gmail.com>
2023-12-05 14:56:38 -0800

    Finished models

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,14 @@
-from django import HttpResponse
+from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello World! Welcomt to the polls index!")
+    return HttpResponse("Hello World! Welcome to the polls index!")
+
+
+def number(request, number, **kwargs):
+    breakpoint()
+    return HttpResponse(
+        f"This is the number route where you picked the number {number}"
+    )
 
 # Create your views here.
